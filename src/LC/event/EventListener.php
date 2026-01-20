751d22a3baefa6989c3d612d492193e3cba74be9
pmmpChris <54825739+PDevChris@users.noreply.github.com>
2026-01-20 18:44:22 -0500

    Refactor EventListener for improved player handling
    Refactor EventListener to use LobbyCore for item management and player teleportation. Update item handling logic for player interactions.

--- src/LC/event/EventListener.php
+++ src/LC/event/EventListener.php
@@ -2,97 +2,75 @@
 
 namespace LC\event;
 
-use LC\api\ItemManager;
-use LC\block\RegisterBlocks;
-use pocketmine\block\Block;
-use pocketmine\block\BlockTypeIds;
-use pocketmine\block\VanillaBlocks;
-use pocketmine\data\bedrock\item\BlockItemIdMap;
+use LC\LobbyCore;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\event\player\PlayerQuitEvent;
-use pocketmine\item\Item;
-use pocketmine\item\ItemIdentifier;
-use pocketmine\item\ItemTypeIds;
-use pocketmine\item\VanillaItems;
-use pocketmine\Server;
-use pocketmine\player\Player;
-use pocketmine\utils\TextFormat as MG;
 use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\item\ItemFactory;
+use pocketmine\player\Player;
+use pocketmine\Server;
 
-use Vecnavium\FormsUI\Form;
-use Vecnavium\FormsUI\FormAPI;
-use Vecnavium\FormsUI\SimpleForm;
-use LC\LobbyCore;
+class EventListener implements Listener {
 
-class EventListener implements Listener
-{
+    private LobbyCore $plugin;
 
-    private $plugin;
-
-    public function onJoin(PlayerJoinEvent $event)
-    {
-
-        $player = $event->getPlayer();
-        $name = $player->getName();
-
-        $event->setJoinMessage("");
+    public function __construct() {
         $this->plugin = LobbyCore::getInstance();
-        Server::getInstance()->broadcastMessage(str_replace(["{player}"], [$player->getName()], "§8[§b+§8]§a{player}"));
-        $player->teleport(Server::getInstance()->getWorldManager()->getDefaultWorld()->getSafeSpawn());
-        $player->getInventory()->clearALL();
-        $player->getArmorInventory()->clearALL();
-
-        $item1 = VanillaBlocks::ENDER_CHEST()->asItem();
-        $item1->setCustomName("§l§f>>§bCosmetics§f<<§r");
-        
-        $item2 = VanillaBlocks::ANVIL()->asItem();
-        $item2->setCustomName("§l§f>>§cReport Player§f<<§r");
-
-        $item3 = VanillaItems::COMPASS();
-        $item3->setCustomName("§l§f>>§aTeleporter§f<<§r");
-
-        $item4 = VanillaItems::BOOK();
-        $item4->setCustomName("§l§f>>§dInformacion§f<<§r");
-
-        $item5 = VanillaItems::NETHER_STAR();
-        $item5->setCustomName("§l§f>>§5Lobby§f<<§r");
-
-
-        $player->getInventory()->setItem(0, $item1);
-        $player->getInventory()->setItem(1, $item2);
-        $player->getInventory()->setItem(4, $item3);
-        $player->getInventory()->setItem(7, $item4);
-        $player->getInventory()->setItem(8, $item5);
     }
 
-    public function onQuit(PlayerQuitEvent $event){
-
+    public function onJoin(PlayerJoinEvent $event) {
         $player = $event->getPlayer();
-        $name = $player->getName();
 
+        // Remove default join message
+        $event->setJoinMessage("");
+
+        // Broadcast custom join message
+        Server::getInstance()->broadcastMessage("§8[§b+§8]§a{$player->getName()}");
+
+        // Teleport player to configured lobby spawn
+        $player->teleport($this->plugin->getLobbySpawn());
+
+        // Clear inventory & armor
+        $player->getInventory()->clearAll();
+        $player->getArmorInventory()->clearAll();
+
+        // Give lobby items from config
+        foreach ($this->plugin->getLobbyItems() as $itemConfig) {
+            $item = ItemFactory::getInstance()->get((int)$itemConfig["id"], 0, 1);
+            $item->setCustomName($itemConfig["name"]);
+            $player->getInventory()->setItem($itemConfig["slot"] - 1, $item);
+        }
+    }
+
+    public function onQuit(PlayerQuitEvent $event) {
+        $player = $event->getPlayer();
         $event->setQuitMessage("");
-        Server::getInstance()->broadcastMessage(str_replace(["{player}"], [$player->getName()], "§8[§c-§8]§c{player}"));
+        Server::getInstance()->broadcastMessage("§8[§c-§8]§c{$player->getName()}");
     }
-	
-    public function onClick(PlayerInteractEvent $event)
-    {
+
+    public function onClick(PlayerInteractEvent $event) {
         $player = $event->getPlayer();
-        $itn = $player->getInventory()->getItemInHand()->getCustomName();
-        if ($itn == "§l§f>>§bCosmetics§f<<§r") {
-            LobbyCore::getInstance()->getUI()->getCosmetics($player);
-        }
-        if ($itn == "§l§f>>§cReport Player§f<<§r"){
-            $this->plugin->getServer()->getCommandMap()->dispatch($player, "report");
-        }
-        if ($itn == "§l§f>>§aTeleporter§f<<§r") {
-            LobbyCore::getInstance()->getUI()->getGames($player);
-        }
-        if ($itn == "§l§f>>§dInformacion§f<<§r") {
-            LobbyCore::getInstance()->getUI()->getInfo($player);
-        }
-        if ($itn == "§l§f>>§5Lobby§f<<§r"){
-            $this->plugin->getServer()->getCommandMap()->dispatch($player, "hub");
+        $itemName = $player->getInventory()->getItemInHand()->getCustomName();
+        $ui = LobbyCore::getUI();
+
+        // Handle item actions
+        switch ($itemName) {
+            case "Boarding Ticket":
+                $ui->getGames($player); // Server selector form
+                break;
+            case "Airport Staff":
+                $ui->getStaffMenu($player); // Staff GUI
+                break;
+            case "Luggage":
+                $ui->getCosmetics($player); // Cosmetics menu
+                break;
+            case "Access Card":
+                $ui->getShop($player); // Shop menu
+                break;
+            case "Your Phone":
+                $ui->getProfile($player); // Profile/friends/inbox
+                break;
         }
     }
 }
