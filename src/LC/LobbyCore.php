81b26bce349eee2530d6deabc00504b545f40e2b
pmmpChris <54825739+PDevChris@users.noreply.github.com>
2026-01-20 18:42:06 -0500

    Update LobbyCore.php

--- src/LC/LobbyCore.php
+++ src/LC/LobbyCore.php
@@ -6,38 +6,45 @@ use pocketmine\Server;
 use pocketmine\player\Player;
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
-use pocketmine\utils\TextFormat as MG;
+use pocketmine\utils\Config;
+use pocketmine\world\Position;
 
 //Events
-
 use LC\event\EventListener;
 use LC\event\Protection;
 
 //Commands
-
 use LC\commands\HubCommand;
 use LC\commands\ItemCommand;
 
-//Uis
-
+//UIs
 use LC\ui\UI;
 
 class LobbyCore extends PluginBase implements Listener {
 
-    private static $instance;
-	
-	public function onLoad() : void {
-		self::$instance = $this;
-	}
+    private static LobbyCore $instance;
+    private Config $config;
+
+    public function onLoad() : void {
+        self::$instance = $this;
+    }
 
     public function onEnable(): void {
         $this->getLogger()->info("§aEnabled LobbyCore");
+
+        // Load config.yml
+        $this->saveDefaultConfig();
+        $this->config = $this->getConfig();
+
+        // Register events
         $this->getServer()->getPluginManager()->registerEvents(new EventListener(), $this);
         $this->getServer()->getPluginManager()->registerEvents(new Protection(), $this);
+
+        // Register commands
         $this->getServer()->getCommandMap()->register("hub", new HubCommand());
-        $this->saveResource("config.yml");
+        $this->getServer()->getCommandMap()->register("item", new ItemCommand());
     }
-    
+
     public function onDisable(): void {
         $this->getLogger()->info("§cDisabled LobbyCore");
     }
@@ -48,5 +55,21 @@ class LobbyCore extends PluginBase implements Listener {
 
     public static function getUI() : UI {
         return new UI();
+    }
+
+    /**
+     * Get lobby spawn location from config
+     */
+    public function getLobbySpawn() : Position {
+        $spawn = $this->config->get("lobby")["spawn"];
+        $world = $this->getServer()->getWorldManager()->getWorldByName($spawn["world"]);
+        return new Position($spawn["x"], $spawn["y"], $spawn["z"], $world, $spawn["yaw"], $spawn["pitch"]);
+    }
+
+    /**
+     * Get items config from config.yml
+     */
+    public function getLobbyItems() : array {
+        return $this->config->get("items");
     }
 }
